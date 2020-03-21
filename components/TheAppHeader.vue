9d0828eff3ae4615fabb33b79ffc4ecc8ba4505a
Vincent Chow <vincent@gromm.co>
2020-03-21 23:45:02 +0800

    Style nav bar

--- components/TheAppHeader.vue
+++ components/TheAppHeader.vue
@@ -1,12 +1,42 @@
 <template>
-  <header class="header">
-    <h1 class="title">Dad Jokes</h1>
-    <ul>
-      <li><nuxt-link to="/">Home</nuxt-link></li>
-      <li><nuxt-link to="/jokes">Jokes</nuxt-link></li>
-      <li><nuxt-link to="/about">About</nuxt-link></li>
-    </ul>
-  </header>
+  <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <span class="font-semibold text-xl tracking-tight">
+        <nuxt-link to="/">Dad Jokes</nuxt-link>
+      </span>
+    </div>
+    <!-- <div class="block lg:hidden">
+      <button
+        class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+      >
+        <svg
+          class="fill-current h-3 w-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
+    </div> -->
+    <div class="flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <nuxt-link to="/jokes" class="mt-4 text-teal-200 hover:text-white mr-4">
+          Jokes
+        </nuxt-link>
+        <nuxt-link to="/about" class="mt-4 text-teal-200 hover:text-white mr-4">
+          About
+        </nuxt-link>
+      </div>
+      <!-- <div>
+        <a
+          href="#"
+          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+          >Download</a
+        >
+      </div> -->
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
